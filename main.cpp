dd2ddee003c1f772cb06d2d7d4e17c97fabd1459
gogo1704 <33185192+gogo1704@users.noreply.github.com>
2018-10-15 20:14:46 +0200

    removed using namespace nana

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
-// http://nanapro.codeplex.com/discussions/544169
-
-//TURN FREAKIN' FOX GAY
-//FAJNY PRZYCISK
 
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/button.hpp>
 int main()
 {
-    using namespace nana;
-    form fm;
+
+    nana::form fm;
     //Define a background for a button.
-    element::bground bground;
+    nana::element::bground bground;
     //Load the BMP file
     //The second parameter is passed by true to indicate that each state aligns vertically.
     //The third parameter specifies the valid area of the picture. It uses the whole picture when the width or height of the valid area is zero.
@@ -18,13 +14,13 @@ int main()
     //Defaultly the button will stretch the whole background of state to fit the widget,
     //but it's easy to make the border deformation occurred. Now we need to specified the pixels
     //of 4 borders, and these border areas will not be deformed when stretching.
-    bground.stretch_parts(20, 20, 20, 20);  //left, top, right, bottom
+    bground.stretch_parts(4, 4, 4, 4);  //left, top, right, bottom
     //Create the button
-    button btn(fm, nana::rectangle(20, 20, 140, 40));
+    nana::button btn(fm, nana::rectangle(20, 20, 140, 40));
     //Set the bground object.
     btn.set_bground(bground);
     fm.show();
-        exec(
+        nana::exec(
 #ifdef NANA_AUTOMATIC_GUI_TESTING
                 1, 1, [&btn]() {click(btn); }
 #endif
