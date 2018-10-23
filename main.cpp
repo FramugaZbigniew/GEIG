4be4941e1c395b6abd85729a06dabf57dda5ca37
FramugaZbigniew <43891392+FramugaZbigniew@users.noreply.github.com>
2018-10-23 21:37:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,76 @@
+#include <wx/wxprec.h>
 
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+class GEIGApp: public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class GEIGFrame: public wxFrame
+{
+public:
+    GEIGFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    wxDECLARE_EVENT_TABLE();
+};
+
+enum
+{
+    ID_Hello = 1
+};
+
+wxBEGIN_EVENT_TABLE(GEIGFrame, wxFrame)
+    EVT_MENU(ID_Hello,   GEIGFrame::OnHello)
+    EVT_MENU(wxID_EXIT,  GEIGFrame::OnExit)
+    EVT_MENU(wxID_ABOUT, GEIGFrame::OnAbout)
+wxEND_EVENT_TABLE()
+
+wxIMPLEMENT_APP(GEIGApp);
+
+bool GEIGApp::OnInit()
+{
+    GEIGFrame *frame = new GEIGFrame( "Hello World", wxPoint(50, 50), wxSize(450, 340) );
+    frame->Show( true );
+    return true;
+}
+
+GEIGFrame::GEIGFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+        : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Generate...\tCtrl-H",
+                     "Used to generate invoices");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append( menuFile, "&File" );
+    menuBar->Append( menuHelp, "&Help" );
+    SetMenuBar( menuBar );
+    CreateStatusBar();
+    SetStatusText( "Welcome to GEIG!" );
+}
+
+void GEIGFrame::OnExit(wxCommandEvent& event)
+{
+    Close( true );
+}
+
+void GEIGFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox( "This is a GEIG' GUI sample",
+                  "About Global Exact Invoice Generator", wxOK | wxICON_INFORMATION );
+}
+
+void GEIGFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Global Exact Invoice Generator");
+}
